198c2c7a82c51f76ca87ba50252e756da775be67
josexrg <josexrg@hotmail.com>
2022-11-07 10:51:58 -0500

    Datadriven and Model added

--- src/test/java/co/com/choucair/certification/academy/stepdefinitions/ChoucairAcademyStepDefinitions.java
+++ src/test/java/co/com/choucair/certification/academy/stepdefinitions/ChoucairAcademyStepDefinitions.java
@@ -4,6 +4,7 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import model.AcademyChoucairData;
 import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
@@ -11,6 +12,8 @@ import questions.Answer;
 import tasks.Login;
 import tasks.OpenUp;
 import tasks.Search;
+
+import java.util.List;
 
 public class ChoucairAcademyStepDefinitions {
 
@@ -20,18 +23,18 @@ public class ChoucairAcademyStepDefinitions {
     }
 
     @Given("^than brandon wants to learn automation at the academy Choucair$")
-    public void thanBrandonWantsToLearnAutomationAtTheAcademyChoucair() {
-        OnStage.theActorCalled("Brandon").wasAbleTo(OpenUp.thePage(), (Login.onThePage()));
+    public void thanBrandonWantsToLearnAutomationAtTheAcademyChoucair(List<AcademyChoucairData> academyChoucairData) {
+        OnStage.theActorCalled("Brandon").wasAbleTo(OpenUp.thePage(), Login.onThePage(academyChoucairData.get(0).getStrUser(), academyChoucairData.get(0).getStrPassword()));
     }
 
-    @When("^he search for the course (.*) on the choucair academy platform$")
-    public void heSearchForTheCourseMetodologíaBancolombiaOnTheChoucairAcademyPlatform(String course) {
-        OnStage.theActorInTheSpotlight().attemptsTo(Search.the(course));
+    @When("^he search for the course on the choucair academy platform$")
+    public void heSearchForTheCourseMetodologíaBancolombiaOnTheChoucairAcademyPlatform(List<AcademyChoucairData> academyChoucairData) {
+        OnStage.theActorInTheSpotlight().attemptsTo(Search.the(academyChoucairData.get(0).getStrCourse()));
     }
 
-    @Then("^he finds the course called (.*)$")
-    public void heFindsTheCourseCalledMetodologíaBancolombia(String question) {
-        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Answer.toThe(question)));
+    @Then("^he finds the course called$")
+    public void heFindsTheCourseCalledMetodologíaBancolombia(List<AcademyChoucairData> academyChoucairData) {
+        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Answer.toThe(academyChoucairData.get(0).getStrCourse())));
     }
 
 }
--- src/test/resources/features/academyChoucair.feature
+++ src/test/resources/features/academyChoucair.feature
@@ -1,9 +1,15 @@
 #Autor: Brandon
-  @stories
-  Feature: Academy Choucair
-    As a user, I want to learn how to automate in screamplay at the Choucair Academy with the automation course
+@stories
+Feature: Academy Choucair
+  As a user, I want to learn how to automate in screamplay at the Choucair Academy with the automation course
   @scenario1
   Scenario: Search for a automation course
     Given than brandon wants to learn automation at the academy Choucair
-    When he search for the course Metodología Bancolombia on the choucair academy platform
-    Then he finds the course called Metodología Bancolombia
+      | strUser    | strPassword   |
+      | 1041232126 | Choucair2022* |
+    When he search for the course on the choucair academy platform
+      | strCourse               |
+      | Metodología Bancolombia |
+    Then he finds the course called
+      | strCourse               |
+      | Metodología Bancolombia |
